50fc414f0a4f21534e767d269622f170888de5da
Bear Cabbage <xiaoxiongbaicai@outlook.com>
2017-12-21 10:49:58 +0800

    Update install.php

--- install.php
+++ install.php
@@ -3,6 +3,12 @@
    Make for Aikuqi
    Cannot use at other ways
  */
+echo ("<form action="welcome.php" method="post">
+IP: <input type="text" name="name"><br>
+User: <input type="text" name="email"><br>
+Passwd:  <input
+<input type="submit">
+</form>")
 $con = mysql_connect("$sqlip","$sqlusr","$sqlpasswd");
 if (!$con)
   {
