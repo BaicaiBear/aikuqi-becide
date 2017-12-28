4ccf738f98e8f7973525ae57eefe7da51d96e883
Bear Cabbage <xiaoxiongbaicai@outlook.com>
2017-12-28 10:38:23 +0800

    Update install.php

--- install.php
+++ install.php
@@ -3,15 +3,10 @@
    Make for Aikuqi
    Cannot use at other ways
  */
-echo ("<form action="welcome.php" method="post">
-IP: <input type="text" name="ip"><br>
-User: <input type="text" name="user"><br>
-Passwd:  <input type="passwd" name="passwd"><br>
-<input type="submit">
-</form>")
-$con = mysql_connect("$sqlip","$sqlusr","$sqlpasswd");
+$con = mysql_connect($_POST["ip"],$_POST["user"],$_POST["passwd"]);
 if (!$con)
   {
-  die('无法连接至数据库: ' . mysql_error());
+  die('Cannot connect: ' . mysql_error());
   }
+else echo("<p>yes</p>");
  
