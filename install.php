4ea549d32458755ea2dd2a059222e12d3fc1ad52
Bear Cabbage <xiaoxiongbaicai@outlook.com>
2018-01-04 10:49:55 +0800

    Update install.php

--- install.php
+++ install.php
@@ -4,13 +4,16 @@
    Cannot use at other ways
  */
  if($POST_['sumbit']==1){
-  $con = mysql_connect($POST_['ip'],$POST_['user'],$POST_['passwd'];
+  $con = mysql_connect('localhost:3306,'root','root';
   if(!$con){
    echo 'cannot connect'.mysql_error.'.';
    echo '<a herf="install.html">Back again</a>';
    die();
   }
   else{
+   $create_database = mysql_query('CREATE DATABASE aikuqidata');
+   if(!$create_database){
+	   $create_table = mysql_query('CREATE TABLE aikuqi_login');
    echo 'OK!';
    echo '<a herf="install2.html">Go on</a>';
    die();
